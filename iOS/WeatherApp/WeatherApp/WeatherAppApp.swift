cca1bff7e15c123b6e6434ce1bbd29fa55320750
Thiago Sena <127417493+thethiagosena@users.noreply.github.com>
2025-01-17 14:26:32 +0000

    add files

---
+++ iOS/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Thiago Sena on 12/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
